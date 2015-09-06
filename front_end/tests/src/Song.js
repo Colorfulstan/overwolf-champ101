59c1efdfc9a9da1e73d00e75ee1eda8aab4f5811
krispin <jonas@krispin.it>
2015-09-06 12:35:26 +0200

    Handle - button styling and basic handle shape
    * font-awesome icons integrated for Menu-icons
    * handlebar shape
    * font and fa sass commit

--- front_end/tests/src/Song.js
+++
@@ -1,8 +0,0 @@
-console.log('Song.js');
-function Song() {
-}
-
-Song.prototype.persistFavoriteStatus = function (value) {
-    // something complicated
-    throw new Error("not yet implemented");
-};
