7be5d19067fb4d59cd0b637de224b8af0b9a1a29
krispin <jonas@krispin.it>
2015-09-11 14:22:39 +0200

    format(directories) changed directory structure

---
+++ front_end/js/Utility/Hotkeys.js
@@ -0,0 +1,48 @@
+// TODO: maybe overthink this file / placement of hotkeys
+
+var can = require('can');
+
+var Hotkeys = function(){};
+Hotkeys.registerHotkeys = function () {
+	overwolf.settings.registerHotKey("toggle_panels", function (result) {
+		steal.dev.log('Hotkey toggle_panels pressed', result);
+		if (result.status == "success") {
+			can.route.attr({'route': 'toggle/all'});
+			steal.dev.log('can route after hitting toggle_panels', can.route.attr());
+		}
+	});
+	var team_handler = function (result, team) {
+		steal.dev.log('Hotkey open_panel_team_' + team + ' pressed', result);
+		if (result.status == "success") {
+			can.route.attr({'route': 'show/:team', 'team': team});
+		}
+	};
+	overwolf.settings.registerHotKey("open_panels_team_blue", function (result) {
+		team_handler(result, 'blue');
+	});
+	overwolf.settings.registerHotKey("open_panels_team_purple", function (result) {
+		team_handler(result, 'purple');
+	});
+	//overwolf.settings.registerHotKey("open_match", function (result) { // TODO: open / close Match implementation - should not reload data but show/hide the matchwindow
+	//	team_handler(result, 'blue');
+	//});
+
+	function create_handler(id){
+		return function close_panel_handler(result) {
+			steal.dev.log('Hotkey close_panel_' + id + ' pressed', result, 'with id:' + id);
+			if (result.status == "success") {
+				can.route.attr({'route': 'close/panel/:id', 'id': id});
+				steal.dev.log('close_panel_handler route nach Hotkey:', can.route.attr());
+			}
+		}
+	}
+	var funcs = [];
+	for (var i = 0; i < 5; i++) {
+		funcs[i] = create_handler(i);
+	}
+	for (var j = 0; j < 5; j++) {
+		overwolf.settings.registerHotKey("close_panel_" + (j+1), funcs[j]);
+		steal.dev.log('registered close_panel_' + (j+1));
+	}
+};
+module.exports = Hotkeys;
--- front_end/stealconfig.js
+++ front_end/stealconfig.js
@@ -5,23 +5,25 @@ System.config({
 		'can/*':'./node_modules/can/*.js',
 		'materialize':'./node_modules/materialize-css/bin/materialize.js',
 
+		'Hotkeys': './js/Utility/Hotkeys.js',
+		'Routes': './js/Utility/Routes.js',
+
 		'SettingsModel': './js/settings/SettingsModel.js',
 		'SettingsCtrl': './js/settings/SettingsCtrl.js',
 
 		'MatchDAO': './js/match/MatchDAO.js',
 		'MatchModel': './js/match/MatchModel.js',
 		'MatchCtrl': './js/match/MatchCtrl.js',
-		'ImageModel': './js/match/ImageModel.js',
+		'ImageModel': './js/match/Elements/ImageModel.js',
+		'SpellModel': './js/match/Elements/SpellModel.js',
 
-		'TooltipCtrl': './js/match/tooltip/TooltipCtrl.js',
-		'OverviewCtrl': './js/match/overview-panel/OverviewCtrl.js',
-		'ChampionCtrl': './js/match/champion-panel/ChampionCtrl.js',
-		'ChampionModel': './js/match/champion-panel/ChampionModel.js',
-		'SpellModel': './js/match/champion-panel/SpellModel.js',
+		'TooltipCtrl': './js/match/Elements/tooltip/TooltipCtrl.js',
+		'OverviewCtrl': './js/match/Elements/overview-panel/OverviewCtrl.js',
+		'ChampionCtrl': './js/match/Elements/champion-panel/ChampionCtrl.js',
+		'ChampionModel': './js/match/Elements/champion-panel/ChampionModel.js',
 
 		'WindowCtrl': './js/WindowCtrl.js',
-		'global': './js/global.js',
-		'Routes': './js/Routes.js',
+		'global': './js/Utility/global.js',
 		'MainCtrl': './js/MainCtrl.js',
 		'FeedbackCtrl': './js/feedback/FeedbackCtrl.js'
 	}
